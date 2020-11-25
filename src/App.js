9e51e8f396b524307cb3eb7dcb231e45fc866526
matiascanton <matiasacanton@gmail.com>
2020-11-25 17:45:15 -0300

    Agregar favorito, eliminar favorito, y guardar en localstorage

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import MovieList from './components/MovieList';
 import MovieListHeader from './components/MovieListHeader';
 import SearchBox from './components/SearchBox';
 import AddFavourite from './components/AddFavourite';
+import RemoveFavourites from './components/RemoveFavourites';
 
 
 const App = () => {
 
-  const [movies, setMovies] = useState ([]);
+  const [movies, setMovies] = useState([]);
+  const [favourites, setFavourites] = useState([]);
   const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async (searchValue) =>{
@@ -35,13 +37,43 @@ const App = () => {
   // Como searchValue cambio, entra useEffect llamando a la funcion getMovieRequest y pasandole el input value
   useEffect(() => {
     getMovieRequest(searchValue);
-    
   }, [searchValue]);
+
+  // Una vez que corre la app, recuperamos los favoritos del Local Storage
+  useEffect(() => {
+    // Convertimos nuestro json que viene del storage en un objeto
+    const movieFavourites = JSON.parse(localStorage.getItem('react-movie-app-favourites'));
+
+    if (movieFavourites) {
+			setFavourites(movieFavourites);
+		}
+  }, []);
+
+  // Agrega la pelicula a favoritos
+  const addFavouriteMovie = (movie) => {
+    //Hace una copia de favoritos y le agregamos la nueva pelicula favorita
+    const newFavouriteList = [...favourites, movie];
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  }
+
+  // Eliminar de Favoritos
+  const removeFavouriteMovie = (movie) => {
+    const newFavouriteList = favourites.filter((favourite) => favourite.imdbID !== movie.imdbID);
+
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  }
+
+  // Guardar en local storage para cuando recargues la pagina queden tus favoritos
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('react-movie-app-favourites', JSON.stringify(items))
+  }
 
   return (
     <div className="container-fluid movie-app" >
       <div className="row d-flex align-items-center mt-4 mb-4">
-        <MovieListHeader heading="Movies"/>
+        <MovieListHeader heading="Peliculas"/>
         <SearchBox
           searchValue = {searchValue}
           setSearchValue = {setSearchValue}
@@ -51,6 +83,17 @@ const App = () => {
         <MovieList
           movies = {movies}
           favouriteComponent = {AddFavourite}
+          handleFavouritesClick = {addFavouriteMovie}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeader heading="Favoritos"/>
+      </div>
+      <div className="row">
+        <MovieList
+          movies = {favourites}
+          favouriteComponent = {RemoveFavourites}
+          handleFavouritesClick = {removeFavouriteMovie}
         />
       </div>
     </div>
